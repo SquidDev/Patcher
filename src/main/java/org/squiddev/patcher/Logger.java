8723ec2d70459d4eb14096289699d641adc5181e
SquidDev <bonzoweb@hotmail.co.uk>
2015-06-12 15:41:17 +0100

    Abstract away logger

--- src/main/java/org/squiddev/patcher/Logger.java
+++ src/main/java/org/squiddev/patcher/Logger.java
@@ -1,18 +1,29 @@
 package org.squiddev.patcher;
 
-/**
- * org.squiddev.patcher (Patcher
- */
 public class Logger {
+	public static Logger instance = new Logger();
+
 	public static void debug(String message) {
-		System.out.println("[DEBUG] " + message);
+		instance.doDebug(message);
 	}
 
 	public static void warn(String message) {
-		System.out.println("[WARN] " + message);
+		instance.doWarn(message);
 	}
 
 	public static void error(String message, Throwable e) {
+		instance.doError(message, e);
+	}
+
+	public void doDebug(String message) {
+		System.out.println("[DEBUG] " + message);
+	}
+
+	public void doWarn(String message) {
+		System.out.println("[WARN] " + message);
+	}
+
+	public void doError(String message, Throwable e) {
 		System.out.println("[ERROR] " + message);
 		e.printStackTrace(System.out);
 	}
